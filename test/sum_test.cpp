1090040833bddb51267e82ce96d2274b99e13578
tmanstrup <thomas.manstrup@gmail.com>
2026-05-12 21:01:35 +0200

    update with error

--- test/sum_test.cpp
+++ test/sum_test.cpp
@@ -15,3 +15,8 @@ TEST(SumTest, SumOfNegativeValues) {
     std::vector<double> values = {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16};
     EXPECT_DOUBLE_EQ(sum(values), -136.0);
 }
+
+TEST(SumTest, SumOfNegativeValuesFail) {
+    std::vector<double> values = {-1, -2, -3, -4, -5, -6, -7, -8, -9, -10, -11, -12, -13, -14, -15, -16};
+    EXPECT_DOUBLE_EQ(sum(values), -134.0);
+}
